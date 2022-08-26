47c9a3d24dc6b29098586eedab091c96ee512324
pwgawzynski <pgawzynski.backup@gmail.com>
2022-08-26 08:36:02 +0200

    Prepared environment to work w. TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 8889,
+      username: 'root',
+      password: 'root',
+      database: 'farm_service',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      bigNumberStrings: false,
+      logging: true,
+      autoLoadEntities: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
