6cfe1cd1d2e76584340773c584160f0355e09064
pwgawzynski <pgawzynski.backup@gmail.com>
2022-08-31 09:03:56 +0200

    Test Mail Send

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { v4 as uuid } from 'uuid';
 import { hashPwd } from '../utils/hash-pwd';
 import { UniversalResponseObject } from '../../types';
+import { MailService } from '../mail/mail.service';
 
 @Injectable()
 export class UserService {
+  constructor(@Inject(MailService) private mailService: MailService) {}
+
   async create(createUserDto: CreateUserDto): Promise<UniversalResponseObject> {
     // TODO check before save if given user do not exist in db already
     if (
@@ -28,6 +31,11 @@ export class UserService {
     user.pwdHashed = hashPwd(createUserDto.password);
     user.age = createUserDto.age;
     user.email = createUserDto.email;
+    /*await this.mailService.sendMail(
+      user.email,
+      'thanks for registration on FarmServiceTM',
+      'click on link below to activate your account',
+    );*/
     user.save();
     return {
       status: true,
