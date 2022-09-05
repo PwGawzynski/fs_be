c33a2c0e34fb2403f80abce801ac8b18c39c5b26
pwgawzynski <pgawzynski.backup@gmail.com>
2022-09-05 08:38:12 +0200

    Added configuration.ts file

---
+++ src/config/configuration.ts
@@ -0,0 +1,13 @@
+export default () => ({
+  authStrategy: {
+    sign:
+      'snigsrouygstiayferouygr8wyagfkyfbr' +
+      'slfogfoguyfsevhewfsduigo7aseyyadusfyua' +
+      'esr6feawgueiwgefydufyufo6ueggiufeywdtu' +
+      'oasuygasguyfuysaroafuyweyftydiawuofoif' +
+      'ausyfuoasfiyroawyfuwfouyasyfsayfyosufo8' +
+      '70ygyft6r5e5zexdhfyrtsw54yufcghdte76ir' +
+      'o8vhkjhkvitydfiycvghfxaerwtdyicghkxfrst' +
+      'e56r6iyufcgctydt67chkvjjl;uiguoifyftyd6i',
+  },
+});
