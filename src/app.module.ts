342c7a6a78a49543065f2313f54c87e2fb63ab0f
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-19 08:08:10 +0200

    Added Company and machine resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
 import { ConfigModule } from '@nestjs/config';
+import { CompanyModule } from './company/company.module';
+import { MachinesModule } from './machines/machines.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -30,6 +32,8 @@ import configuration from './config/configuration';
       isGlobal: true,
       load: [configuration],
     }),
+    CompanyModule,
+    MachinesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/machines/machines.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMachineDto } from './dto/create-machine.dto';
+import { UpdateMachineDto } from './dto/update-machine.dto';
+
+@Injectable()
+export class MachinesService {}
