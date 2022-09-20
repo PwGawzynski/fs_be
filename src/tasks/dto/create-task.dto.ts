f3eb136dd5923a4387ca35d1b597725604e89678
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-20 22:40:40 +0200

    added DTO and simple validation

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,25 @@
-export class CreateTaskDto {}
+import {
+  IsDate,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString({ message: 'Task name nus be a string type' })
+  @MaxLength(300)
+  @IsNotEmpty({ message: 'Task name cannot be empty' })
+  name: string;
+
+  @IsString({
+    message: 'Task description must be a string type',
+  })
+  @MaxLength(1000)
+  @IsOptional()
+  description: string;
+
+  @IsDate({ message: `Task's Performance day must be a string type` })
+  @IsNotEmpty({ message: `Task's performance data myst be given` })
+  performanceDay: Date;
+}
