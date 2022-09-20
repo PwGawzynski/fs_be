7cd41079b9ddcf774b0cdfc2650ca2a29d2ae4f6
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-20 19:15:22 +0200

    added HttpModule

--- src/field/field.module.ts
+++ src/field/field.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { FieldService } from './field.service';
 import { FieldController } from './field.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+  ],
   controllers: [FieldController],
   providers: [FieldService],
 })
