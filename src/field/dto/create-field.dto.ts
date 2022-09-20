ad670e5502bd7365d67416bb78fa4812c71644cd
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-20 19:13:22 +0200

    Implemented validation for req data and created DTO for reqs

--- src/field/dto/create-field.dto.ts
+++ src/field/dto/create-field.dto.ts
@@ -1,1 +1,17 @@
-export class CreateFieldDto {}
+import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
+
+export class CreateFieldDto {
+  @IsNotEmpty()
+  @IsNumber({
+    maxDecimalPlaces: 7,
+  })
+  latitude: number;
+  @IsNotEmpty()
+  @IsNumber({
+    maxDecimalPlaces: 7,
+  })
+  longitude: number;
+
+  @IsOptional()
+  name: string;
+}
