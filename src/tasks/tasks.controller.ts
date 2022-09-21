2e2232b3c279ba4ecdbc89b00fd4321493d8b9e0
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-21 09:58:37 +0200

    created Post action to addNewTask

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UserObj } from '../decorators/user-obj.decorator';
+import { User } from '../user/entities/user.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../Guards/RolesGuard';
+import { AllowOnlyIf } from '../decorators/AllowOnlyIf.decorator';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @AllowOnlyIf('client', 'owner')
+  createNew(@Body() data: CreateTaskDto, @UserObj() user: User) {
+    return this.tasksService.createNewTask(data, user);
+  }
 }
