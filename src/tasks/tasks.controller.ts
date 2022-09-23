a85587367555204286b0730a93636011752982f1
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-23 11:52:51 +0200

    some tests 've been done before fixing bug [query error crashes nest]

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, Put, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Put,
+  Res,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UserObj } from '../decorators/user-obj.decorator';
@@ -7,6 +15,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from '../Guards/RolesGuard';
 import { AllowOnlyIf } from '../decorators/AllowOnlyIf.decorator';
 import { UpdateTaskAddWorkersDto } from './dto/update-task.dto';
+import { Response } from 'express';
 
 @Controller('tasks')
 export class TasksController {
@@ -15,8 +24,12 @@ export class TasksController {
   @Post()
   @UseGuards(AuthGuard('jwt'), RolesGuard)
   @AllowOnlyIf('client', 'owner')
-  createNew(@Body() data: CreateTaskDto, @UserObj() user: User) {
-    return this.tasksService.createNewTask(data, user);
+  createNew(
+    @Body() data: CreateTaskDto,
+    @UserObj() user: User,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return this.tasksService.createNewTask(data, user, res);
   }
 
   @Put('/add-workers')
