59e74c2fbe7ed7d2d596aea12f0bb358029e7c44
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-27 16:05:26 +0200

    moved methods to separated second abstraction layer ( helper-service) and 3-th abstraction layer ( db-validator )

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,53 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { Company } from './entities/company.entity';
 import { User } from '../user/entities/user.entity';
 import { UniversalResponseObject } from '../../types';
+import { CompanyHelperService } from './company-helper/company-helper.service';
 
 @Injectable()
 export class CompanyService {
-  private static async isUniqueEntity(
-    field: string,
-    value: string,
-  ): Promise<boolean | UniversalResponseObject> {
-    return (
-      await Company.find({
-        where: {
-          [field]: value,
-        },
-      })
-    ).length
-      ? ({
-          status: false,
-          message: `Given ${field} is not unique`,
-        } as UniversalResponseObject)
-      : true;
-  }
-
-  private static async _assignDataToEntity(
-    data: CreateCompanyDto,
-    user: User,
-  ): Promise<Company | UniversalResponseObject> {
-    const company = new Company();
-
-    // check if given company name is unique
-    const validationNameResult = await CompanyService.isUniqueEntity(
-      'name',
-      data.name,
-    );
-    if (!(typeof validationNameResult === 'boolean'))
-      return validationNameResult;
-
-    company.name = data.name;
-    company.description = data.description ? data.description : null;
-    company.machines = null;
-    company.owners = [user];
-    company.tasks = null;
-    return company;
-  }
+  constructor(
+    @Inject(CompanyHelperService)
+    private readonly companyHelperService: CompanyHelperService,
+  ) {}
 
   async createCompany(data: CreateCompanyDto, user: User) {
-    const createdCompany = await CompanyService._assignDataToEntity(data, user);
+    const createdCompany = await this.companyHelperService.assignDataToNew(
+      data,
+      user,
+    );
     console.log(createdCompany);
     if (!(createdCompany instanceof Company)) return createdCompany;
     return createdCompany
