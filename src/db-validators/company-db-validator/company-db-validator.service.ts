c79c3eac2b24b138ac4042e59d926e60ee46e4e8
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-27 22:11:47 +0200

    added method to check if given user is owner of worker's company

--- src/db-validators/company-db-validator/company-db-validator.service.ts
+++ src/db-validators/company-db-validator/company-db-validator.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { UniversalResponseObject } from '../../../types';
 import { Company } from '../../company/entities/company.entity';
+import { CreateWorkerDto } from '../../worker/dto/create-worker.dto';
+import { User } from '../../user/entities/user.entity';
 
 @Injectable()
 export class CompanyDbValidatorService {
@@ -38,5 +40,32 @@ export class CompanyDbValidatorService {
         message: 'Cannot find given company',
       } as UniversalResponseObject;
     return company;
+  }
+
+  public async isOwner(
+    data: CreateWorkerDto,
+    user: User,
+  ): Promise<Company | UniversalResponseObject> {
+    const companyGivenToBeSigned = await Company.findOne({
+      where: {
+        id: data.companyID,
+      },
+      relations: ['owners'],
+    });
+    if (!companyGivenToBeSigned)
+      return {
+        status: false,
+        message: `Company with given id doesn't exist`,
+      } as UniversalResponseObject;
+
+    if (
+      !companyGivenToBeSigned.owners.filter((owner) => owner.id === user.id)
+        .length
+    )
+      return {
+        status: false,
+        message: 'You are not owner of this company',
+      } as UniversalResponseObject;
+    return companyGivenToBeSigned;
   }
 }
