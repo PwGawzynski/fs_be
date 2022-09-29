b8808869a0a2ce40274244fa8a2007199f512157
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-29 09:40:48 +0200

    rem because column now has unique attribute and this check is not obligatory

--- src/db-validators/field-db-validator/field-db-validator.service.ts
+++ src/db-validators/field-db-validator/field-db-validator.service.ts
@@ -1,24 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { UniversalResponseObject } from '../../../types';
-import { Field } from '../../field/entities/field.entity';
 
 @Injectable()
-export class FieldDbValidatorService {
-  public async isUniqueEntity(
-    field: string,
-    value: string,
-  ): Promise<boolean | UniversalResponseObject> {
-    return (
-      await Field.find({
-        where: {
-          [field]: value,
-        },
-      })
-    ).length
-      ? ({
-          status: false,
-          message: `Given ${field} is not unique`,
-        } as UniversalResponseObject)
-      : true;
-  }
-}
+export class FieldDbValidatorService {}
