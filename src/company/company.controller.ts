54aa7f5c940e34077690fdd8dc8573b01c61efb1
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-17 21:49:02 +0200

    added post for create

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
+import { UserObj } from '../decorators/user-obj.decorator';
+import { User } from '../user/entities/user.entity';
 import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from '../Guards/RolesGuard';
 import { AllowOnlyIf } from '../decorators/AllowOnlyIf.decorator';
-import { UserObj } from '../decorators/user-obj.decorator';
-import { User } from '../user/entities/user.entity';
 
 @Controller('company')
 export class CompanyController {
@@ -14,7 +14,11 @@ export class CompanyController {
   @Post()
   @UseGuards(AuthGuard('jwt'), RolesGuard)
   @AllowOnlyIf('owner')
-  createNew(@Body() data: CreateCompanyDto, @UserObj() user: User) {
-    return this.companyService.createCompany(data, user);
+  create(@Body() data: CreateCompanyDto, @UserObj() user: User) {
+    try {
+      return this.companyService.create(data, user);
+    } catch (e) {
+      throw e;
+    }
   }
 }
