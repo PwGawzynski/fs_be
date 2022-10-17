e8dcb302c88d083f5f26c9ae9780021c9fd73885
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-17 21:50:29 +0200

    changed logic

--- src/field/field.controller.ts
+++ src/field/field.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { FieldService } from './field.service';
-import { CreateFieldDto } from './dto/create-field.dto';
-import { User } from '../user/entities/user.entity';
-import { UserObj } from '../decorators/user-obj.decorator';
 import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from '../Guards/RolesGuard';
 import { AllowOnlyIf } from '../decorators/AllowOnlyIf.decorator';
+import { CreateFieldDto } from './dto/create-field.dto';
+import { UserObj } from '../decorators/user-obj.decorator';
+import { User } from '../user/entities/user.entity';
 
 @Controller('field')
 export class FieldController {
@@ -13,8 +13,8 @@ export class FieldController {
 
   @Post()
   @UseGuards(AuthGuard('jwt'), RolesGuard)
-  @AllowOnlyIf('owner')
-  setUpNewField(@Body() data: CreateFieldDto, @UserObj() user: User) {
-    return this.fieldService.create(data, user);
+  @AllowOnlyIf('owner', 'client')
+  create(@Body() data: CreateFieldDto, @UserObj() user: User) {
+    return this.fieldService.createNewField(data, user);
   }
 }
