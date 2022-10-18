54787cca4c79c819d822fcedbfaf527096545f6d
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-18 11:43:07 +0200

    added new method for getting daily statistic for given user or for req causer

--- src/statistic/statistic.controller.ts
+++ src/statistic/statistic.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseEnumPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { StatisticService } from './statistic.service';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../Guards/RolesGuard';
+import { AllowOnlyIf } from '../decorators/AllowOnlyIf.decorator';
+import { WorkerIdToEntity } from '../pipes/workerIdToEntity.pipe';
+import { Worker } from '../worker/entities/worker.entity';
+import { UserObj } from '../decorators/user-obj.decorator';
+import { User } from '../user/entities/user.entity';
+import { UserRole } from '../../types';
 
 @Controller('statistic')
 export class StatisticController {
   constructor(private readonly statisticService: StatisticService) {}
+
+  @Get(':role/daily-done-task/:workerId?')
+  @UseGuards(AuthGuard('jwt'), RolesGuard)
+  @AllowOnlyIf('owner', 'worker')
+  getDailyStats(
+    @Param('role', new ParseEnumPipe(UserRole)) role: UserRole,
+    @UserObj() user: User,
+    @Param('workerId', WorkerIdToEntity) worker?: Worker | undefined,
+  ) {
+    return this.statisticService.getDailyDoneTask(worker, user, role);
+  }
 }
