937cb5bd8ad29f87cae1698985225b32e19fd3b5
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-20 23:36:16 +0200

    created nap dtp

--- src/nap/dto/create-nap.dto.ts
+++ src/nap/dto/create-nap.dto.ts
@@ -1,1 +1,30 @@
-export class CreateNapDto {}
+import { IsDate, IsNotEmpty, IsOptional } from 'class-validator';
+import { FindOrReject } from '../../ClassValidatorCustomDecorators/FindOrReject.decorator';
+import { Worker } from '../../worker/entities/worker.entity';
+
+export class CreateNapDto {
+  @IsOptional()
+  @IsNotEmpty({
+    message: 'Start date must not be empty if it is given',
+  })
+  @IsDate({
+    message: 'Start date param must be date type',
+  })
+  startDate?: Date;
+
+  @IsOptional()
+  @IsNotEmpty({
+    message: 'End date must not be empty if it is given',
+  })
+  @IsDate({
+    message: 'End date param must be date type',
+  })
+  endDate?: Date;
+
+  @IsOptional()
+  @IsNotEmpty({
+    message: 'Worker Id must not be empty if it is given',
+  })
+  @FindOrReject(Worker)
+  workDayId?: string;
+}
