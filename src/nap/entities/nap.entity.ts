7cca2a73379f7ad6fa74b7d17881dd92f8c764a5
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-21 11:24:35 +0200

    added throwable exception method with check if for given day is not open any nap and trow err if it is

--- src/nap/entities/nap.entity.ts
+++ src/nap/entities/nap.entity.ts
@@ -1,12 +1,15 @@
 import {
   BaseEntity,
+  Between,
   Column,
   Entity,
+  IsNull,
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { WorkDay } from '../../work-day/entities/work-day.entity';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Entity()
 export class Nap extends BaseEntity {
@@ -25,4 +28,41 @@ export class Nap extends BaseEntity {
   @ManyToOne(() => WorkDay, (workDay) => workDay.naps)
   @JoinColumn()
   workDay: Promise<WorkDay>;
+
+  public static async checkIfAnyIsOpen(startDate: Date) {
+    if (
+      !!(await Nap.findOne({
+        where: {
+          startDate: Between(
+            new Date(
+              new Date(
+                startDate.getFullYear(),
+                startDate.getMonth(),
+                startDate.getDate(),
+                0,
+              ).setHours(0),
+            ),
+            new Date(
+              new Date(
+                startDate.getFullYear(),
+                startDate.getMonth(),
+                startDate.getDate(),
+                23,
+                59,
+                59,
+                999,
+              ),
+            ),
+          ),
+          endDate: IsNull(),
+        },
+      }))
+    ) {
+      throw new HttpException(
+        'There is open nap for given date, close or delete it and try again',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+    return true;
+  }
 }
