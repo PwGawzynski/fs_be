6e1a3e474dc07f8471422fe820f6cc634de3fe59
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-21 19:51:33 +0200

    added method for get open work day and close work day for currently open and given ( when causer is owner )

--- src/work-day/work-day.service.ts
+++ src/work-day/work-day.service.ts
@@ -7,6 +7,9 @@ import { Worker } from '../worker/entities/worker.entity';
 import { TypeORMError } from 'typeorm';
 import { CompanyService } from '../company/company.service';
 import { Company } from '../company/entities/company.entity';
+import { GetWorkDayDto } from './dto/get-work-day.dto';
+import { CloseWorkDayDto } from './dto/close-work-day.dto';
+import { GetDatesBetweenForQuery } from '../utils/beetwen-dates';
 
 @Injectable()
 export class WorkDayService {
@@ -60,6 +63,92 @@ export class WorkDayService {
         ? CheckDateOption.ForGivenDay
         : CheckDateOption.ForPreviousDay,
     );
+
+    return workDay
+      .save()
+      .then(() => {
+        return { status: true } as UniversalResponseObject;
+      })
+      .catch(() => {
+        throw new TypeORMError('Cannot save');
+      });
+  }
+
+  async getOpenWorkDay(role: UserRole, data: GetWorkDayDto, user: User) {
+    const workDay = new WorkDay();
+    switch (role) {
+      case UserRole.worker:
+        workDay.worker = Worker.findOne({
+          where: {
+            user: {
+              id: user.id,
+            },
+          },
+        });
+        if (!(await workDay.worker))
+          throw new HttpException(
+            'Causer is not worker of anny company',
+            HttpStatus.BAD_REQUEST,
+          );
+        workDay.doneForCompany = (await workDay.worker).isWorkerAtCompany;
+        await workDay.findForAndFill(
+          CheckDateOption.ForPreviousDay,
+          'Any open work day dont exist',
+        );
+        break;
+      case UserRole.owner:
+        await this.companyService.checkIfOwner(
+          await (data.workerId as unknown as Worker).isWorkerAtCompany,
+          user,
+        );
+        if (!data.startDate)
+          throw new HttpException(
+            'Start Date param must be given for ask as owner',
+            HttpStatus.BAD_REQUEST,
+          );
+        workDay.startDate = data.startDate;
+        workDay.worker = Promise.resolve(data.workerId as unknown as Worker);
+        workDay.doneForCompany = (
+          data.workerId as unknown as Worker
+        ).isWorkerAtCompany;
+        await workDay.findForAndFill(
+          CheckDateOption.ForGivenDay,
+          'Cannot find any open work day for given params',
+        );
+    }
+
+    return {
+      status: true,
+      data: {
+        startDate: workDay.startDate,
+        id: workDay.id,
+        naps: await workDay.naps,
+      },
+    } as UniversalResponseObject;
+  }
+
+  async closeWorkDay(role: UserRole, data: CloseWorkDayDto, user: User) {
+    let workDay;
+    switch (role) {
+      case UserRole.worker:
+        workDay = await WorkDay.findOne({
+          where: {
+            worker: {
+              user: {
+                id: user.id,
+              },
+            },
+            startDate: GetDatesBetweenForQuery(new Date(), new Date()),
+          },
+        });
+        if (!workDay)
+          throw new HttpException(
+            'Cannot find work day open for today connect with causer',
+            HttpStatus.BAD_REQUEST,
+          );
+        workDay.endDate = new Date();
+        break;
+    }
 
     return workDay
       .save()
